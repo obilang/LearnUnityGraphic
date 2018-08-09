6b7ca02242311cfd2fa60eebacfadd5eda3cecdb
Gu Wei <guwei@virtuosgames.com>
2018-08-09 15:09:58 +0800

    Blend two image

---
+++ LearnGrapic/Assets/Scripts/DecalOnOff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class DecalOnOff : MonoBehaviour
+{
+    Material mat;
+    bool showDecal = false;
+
+    private void OnMouseDown()
+    {
+        showDecal = !showDecal;
+        if (showDecal)
+        {
+            mat.SetFloat("_ShowDecal", 1);
+        }
+        else
+        {
+            mat.SetFloat("_ShowDecal", 0);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        mat = this.GetComponent<Renderer>().sharedMaterial;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
